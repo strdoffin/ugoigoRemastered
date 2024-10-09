aab4eada1d5120d54036a53c87682daa1d4c54b7
strdoffin <undefu5624@gmail.com>
2024-10-09 18:38:21 +0700

    Update page.tsx

--- ugoigos/app/feed/page.tsx
+++ ugoigos/app/feed/page.tsx
@@ -1,8 +1,22 @@
+'use client'
 import TripCard from "@/components/TripCard";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import Image from "next/image";
+import { faChevronCircleLeft } from '@fortawesome/free-solid-svg-icons'
+import {useRouter} from "next/navigation";
 export default function Page() {
+    const router = useRouter();
     return (
-        <div className="flex flex-col h-screen max-w-md mx-auto items-center justify-center rounded-xl bg-gradient-to-b from-[#C4E4E0] to-[#50988E] overflow-hidden">
-            <div className="flex flex-col w-full h-full overflow-y-auto p-4">
+        <div className="flex flex-col h-screen max-w-md mx-auto items-center justify-center rounded-xl bg-gradient-to-b from-[#C4E4E0] to-[#50988E] overflow-hidden ">
+            
+            <div className="w-full bg-[#579D93] p-4 rounded-t-lg mb-4 flex items-center">
+                    <button onClick={() => { router.back() }} className="">
+                        <FontAwesomeIcon icon={faChevronCircleLeft} className="text-white text-2xl" />
+                    </button>
+                    <h1 className="text-4xl text-white text-center flex-[0.95]">UGOIGO</h1>
+                </div>
+                
+            <div className="flex flex-col gap-6 w-full h-full overflow-y-auto p-4">
                 <TripCard
                     title="Siam Center"
                     location="Bangkok"
